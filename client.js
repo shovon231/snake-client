35450e5d67227f556dd5dd9310ebe5ad26126484
Mohammad Razaul Islam <shovon231@gmail.com>
2023-02-26 19:26:56 -0500

    first commit

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", (data) => {
+    console.log("hey hey", data);
+  });
+  conn.on("");
+  conn.on("connect", () => {
+    conn.write("Hello from client!");
+  });
+  return conn;
+};
+
+module.exports = connect;
