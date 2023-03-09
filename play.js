509a1a9732e65fbaa39de1be9d9501686cc1541c
Mohammad Razaul Islam <shovon231@gmail.com>
2023-03-09 16:02:41 +0000

    Refactoring the play.js file by creating new input.js file and moved the setupInput function

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 console.log("Connecting ...");
 let connection = connect();
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  // your code here
-  // \u0003 maps to ctrl+c input
-  if (key === "\u0003") {
-    process.exit();
-  }
-};
 setupInput();
-module.exports = { setupInput };
-
