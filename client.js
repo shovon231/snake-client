6076f8aa7dd9b591e43433a98c5c9c45a5605b2f
Mohammad Razaul Islam <shovon231@gmail.com>
2023-03-09 15:52:44 +0000

    adding functionality of ctrl+c within setupInput and handleUserInput

--- client.js
+++ client.js
@@ -1,28 +1,35 @@
 const net = require("net");
+const stdin = require("process");
+const { setupInput } = require("./play");
+
 const connect = function () {
   const conn = net.createConnection({
     host: "localhost", // IP address here,
     port: 50541, // PORT number here,
   });
-
-  //interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
+  let interval = 0;
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
-  });
-  let intervalID;
-  conn.on("connect", () => {
-    intervalID = setInterval(() => {
+    conn.write(`Name: snk`);
+    setInterval(() => {
       //conn.write("Move: up");
+    }, 500);
+
+    setTimeout(() => {
+      conn.write("Move: left");
     }, 1000);
+    setTimeout(() => {
+      conn.write("Move: down");
+    }, 2000);
+    setTimeout(() => {
+      conn.write("Move: right");
+    }, 3000);
   });
-  setTimeout(() => {
-    clearInterval(intervalID);
-  }, 3000);
+
+  // Define encoding
+  conn.setEncoding("utf8");
+  conn.on("data", (stream) => console.log(stream));
+
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,3 +1,23 @@
 const { connect } = require("./client");
 console.log("Connecting ...");
-connect();
+let connection = connect();
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (key) {
+  // your code here
+  // \u0003 maps to ctrl+c input
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+setupInput();
+module.exports = { setupInput };
+
